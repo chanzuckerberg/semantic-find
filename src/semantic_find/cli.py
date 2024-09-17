40684da6d321f9790608f8dbfad28d1fa0c31f94
Ryan Lim <rzlim08@gmail.com>
2024-09-17 14:03:29 -0700

    add insert function

--- src/semantic_find/cli.py
+++ src/semantic_find/cli.py
@@ -5,13 +5,24 @@ import semantic_find
 def main():
     """Console script for semantic_find."""
     parser = argparse.ArgumentParser(description="Console script for semantic_find.")
-    # Add arguments here, for example:
-    # parser.add_argument('arg_name', type=str, help='Description of the argument')
-
-    parser.add_argument("query", type=str, help="Query to search for")
-
+    
+    # Adding subcommands: 'search' and 'insert'
+    subparsers = parser.add_subparsers(dest="command", help="Subcommands")
+    
+    # Subcommand for search
+    search_parser = subparsers.add_parser("search", help="Search for a query")
+    search_parser.add_argument("query", type=str, help="Query to search for")
+    
+    # Subcommand for insert
+    insert_parser = subparsers.add_parser("insert", help="Insert a new entry")
+    
     args = parser.parse_args()
-    semantic_find.search(args.query)
+
+    # Handling the commands
+    if args.command == "search":
+        semantic_find.search(args.query)
+    elif args.command == "insert":
+        semantic_find.insert()
     
 
 if __name__ == "__main__":
--- src/semantic_find/semantic_find.py
+++ src/semantic_find/semantic_find.py
@@ -1,4 +1,35 @@
+import os
+import psycopg2
+import numpy as np
+from pgvector.psycopg2 import register_vector
+
+
+
+POSTGRES_USER=os.environ.get("POSTGRES_USER", "sf_user")
+POSTGRES_PASSWORD=os.environ.get("POSTGRES_USER", "sf_password")
+POSTGRES_DB=os.environ.get("POSTGRES_USER", "semantic_find")
+POSTGRES_HOST=os.environ.get("POSTGRES_HOST", "localhost")
+POSTGRES_PORT=os.environ.get("POSTGRES_PORT", 5434)
+
+
 """Main module."""
 def search(query: str) -> None:
     """Search for the given query."""
     print(f"Searching for {query}...")
+
+
+def insert():
+    conn = psycopg2.connect(host=POSTGRES_HOST, port=POSTGRES_PORT, user=POSTGRES_USER, password=POSTGRES_PASSWORD, dbname=POSTGRES_DB)
+    register_vector(conn)
+
+    cursor = conn.cursor()
+    test_embedding = np.random.rand(1024)
+    cursor.execute("""
+        INSERT INTO vector_embeddings (content, tokens, embedding) 
+                   VALUES (%s, %s, %s)
+                   """, 
+                   ("test", 1, test_embedding)
+    )
+    cursor.close()
+    conn.commit()
+    
